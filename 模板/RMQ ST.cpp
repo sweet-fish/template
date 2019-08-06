59909f9c2304fbb5a54325e6131c20005f679b40
sweet_fish <34713201+sweet-fish@users.noreply.github.com>
2019-08-07 02:42:00 +0800

    Update RMQ ST.cpp

--- 模板/RMQ ST.cpp
+++ 模板/RMQ ST.cpp
@@ -11,8 +11,19 @@ void init(int n)
         for(int i=1;i+(1<<j)-1<=n;++i)
             st[i][j]=max(st[i][j-1],st[i+(1<<(j-1))][j-1]);
 }
+void init0(int n)
+{
+    for(int i=1;i<=n;++i)
+        len[i]=log(i)/log(2);
+    for(int i=0;i<tot;i++)
+        st[i][0]=a[i];
+    for(int j=1;(1<<j)<=n;j++)
+        for(int i=0;i+(1<<j)-1<n;i++)
+            f[i][j]=max(f[i][j-1],f[i+(1<<(j-1))][j-1]);
+}
 int query(int l,int r)
 {
     int k=len[r-l+1];
     return max(st[l][k],st[r-(1<<k)+1][k]);
 }
+
