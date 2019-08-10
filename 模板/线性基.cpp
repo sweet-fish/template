45d99a724f971d4607db01de6af8edf256adb1b3
sweet_fish <34713201+sweet-fish@users.noreply.github.com>
2019-08-10 20:54:11 +0800

    Update 线性基.cpp

--- 模板/线性基.cpp
+++ 模板/线性基.cpp
@@ -5,7 +5,7 @@ struct LB
     LB(){mem(base,0);sz=0;tol=0;}
     bool ins(ll v)
     {
-        for(int i=60;~i;--i)
+        for(int i=63;~i;--i)
             if(v&(1LL<<i))
             {
                 if(!base[i])
@@ -22,7 +22,7 @@ struct LB
     }
     void rebuild()
     {
-        for(int i=60;~i;--i)
+        for(int i=63;~i;--i)
             for(int j=i-1;~j;--j)
                 if(base[i]&(1LL<<j))
                     base[i]^=base[j];
@@ -31,12 +31,12 @@ struct LB
     {
         if(k>=(1LL<<sz)||k<0)
             return -1;
-        ll tp[60];
+        ll tp[64];
         int cnt=0;
-        for(int i=0;i<60;++i)
+        for(int i=0;i<64;++i)
             if(base[i])
                 tp[cnt++]=base[i];
-        for(int i=60;~i;--i)
+        for(int i=63;~i;--i)
             if(k&(1LL<<i))
                 v^=tp[i];
         return v;
@@ -45,18 +45,29 @@ struct LB
     {
         return kth(v,(1LL<<sz)-1);
     }
-
+    ll rnk(ll v)
+    {
+        //整个序列中任选(可为空)，可组成的异或值，每个都出现pow(2,tol-sz)次
+        ll r=0;
+        for(int i=0;i<64;++i)
+        {
+            if(base[i])
+                if(v&(1LL<<i))
+                    r+=(1<<i);
+        }
+        return r;
+    }
     LB join(const LB &t)
     {
         LB r;
-        for(int i=0;i<60;++i)
+        for(int i=0;i<64;++i)
             r.ins(base[i]),r.ins(t.base[i]);
         return r;
     }
     LB inner(const LB &t)
     {
         LB c=t,d=t,res;
-        for(int i=60;~i;--i)
+        for(int i=63;~i;--i)
         {
             if(!base[i])
                 continue;
@@ -82,7 +93,7 @@ struct LB
     void show()
     {
         cout<<sz<<endl;
-        for(int i=0;i<60;++i)
+        for(int i=0;i<64;++i)
             if(base[i])
             cout<<i<<' '<<(bitset<50>)base[i]<<endl;
     }
